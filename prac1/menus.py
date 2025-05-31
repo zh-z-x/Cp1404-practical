6ca37e5acbdfb40ea831f79dbf3825e980f3ff27
zzx <zixin.zhang@my.jcu.edu.au>
2025-05-31 12:10:01 +0800

    menus

---
+++ prac1/menus.py
@@ -0,0 +1,19 @@
+name = input("Enter name: ")
+
+MENU = """(H)ello
+(G)oodbye
+(Q)uit"""
+print(MENU)
+choice = input(">>> ").upper()
+
+while choice != "Q":
+    if choice == "H":
+        print(f"Hello {name}")
+    elif choice == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    print(MENU)
+    choice = input(">>> ").upper()
+
+print("Finished.")
