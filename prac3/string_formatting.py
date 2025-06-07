edb65d2f707419b851597dbfe3126a542a7efe3c
zzx <zixin.zhang@my.jcu.edu.au>
2025-06-07 11:00:14 +0800

    string_formatting

---
+++ prac3/string_formatting.py
@@ -0,0 +1,45 @@
+"""
+CP1404/CP5632 - Practical
+Various examples of using Python string formatting.
+(We prefer f-strings in this subject.)
+Want to read more about it?
+https://docs.python.org/3/library/string.html#formatstrings
+"""
+
+name = "Gibson L-5 CES"
+year = 1922
+cost = 16035.9
+
+# The 'old' manual way to format text with string concatenation (don't do this):
+print("My guitar: " + name + ", first made in " + str(year))
+
+# A better way - using str.format() (don't do this unless you need to):
+print("My guitar: {}, first made in {}".format(name, year))
+print("My guitar: {0}, first made in {1}".format(name, year))
+print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
+
+# And with f-string formatting, introduced in Python 3.6 (do this)
+print(f"My {name} was first made in {year} (that's right, {year}!)")
+
+# Formatting currency (grouping with comma, 2 decimal places):
+print("My {} would cost ${:,.2f}".format(name, cost))  # str.format version
+print(f"My {name} would cost ${cost:,.2f}")  # preferred f-string version
+
+# Aligning columns by using width after the :
+# This loop uses enumerate, which is useful when you want both the index and value
+numbers = [1, 19, 123, 456, -25]
+
+for i, number in enumerate(numbers, 1):
+    print(f"Number {i} is {number:5}")
+
+# TODO: Use f-string formatting to produce the output:
+# 1922 Gibson L-5 CES for about $16,036!
+print(f"{year} {name} for about ${cost:,.0f}!")
+
+# TODO: Using a for loop with the range function and f-string formatting,
+# produce the following right-aligned output (DO NOT use a list):
+# 2 ^ 0 is    1
+# ...
+# 2 ^10 is 1024
+for i in range(11):
+    print(f"2 ^ {i:2} is {2 ** i:4}")
