cb7c06c04d8dbf463db676195e5872107e28b77a
zzx <zixin.zhang@my.jcu.edu.au>
2025-06-21 11:04:06 +0800

    Implement input reading, word splitting, and word count dictionary initialization

---
+++ prac5/word_occurrences.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter a string
+text = input("Text: ")
+
+# Split the input text into a list of words
+words = text.split()
+
+# Create a dictionary to count occurrences of each word
+word_counts = {}
