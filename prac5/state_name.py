3679b6d2bc17d49237eb51c331b059e77d46495d
zzx <zixin.zhang@my.jcu.edu.au>
2025-06-21 10:44:57 +0800

    Reformatted dictionary to follow PEP 8 style

---
+++ prac5/state_name.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+"""
+
+# Reformatted dictionary to follow PEP 8 style
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
