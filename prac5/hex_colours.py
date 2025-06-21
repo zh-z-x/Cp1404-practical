114e19b6463a99c5522c52e55608e8ec8e08da69
zzx <zixin.zhang@my.jcu.edu.au>
2025-06-21 10:55:41 +0800

    Constant dictionary mapping color names to hexadecimal codes

---
+++ prac5/hex_colours.py
@@ -0,0 +1,13 @@
+# Constant dictionary mapping color names to hexadecimal codes
+COLOR_TO_HEX = {
+    "crimson": "#dc143c",
+    "coral": "#ff7f50",
+    "darkorchid": "#9932cc",
+    "deepskyblue": "#00bfff",
+    "forestgreen": "#228b22",
+    "gold": "#ffd700",
+    "hotpink": "#ff69b4",
+    "indigo": "#4b0082",
+    "khaki": "#f0e68c",
+    "lavender": "#e6e6fa"
+}
