56b578b04df76c3595235be7d3781833ef47461f
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-05 12:39:17 +0800

    Added: __init__ method with 4 attributes; name field; typing field; reflection field; year field.

---
+++ prac6/programming_language.py
@@ -0,0 +1,14 @@
+"""
+programming_language.py
+
+Estimated time: 30 minutes
+
+Actual time: minutes
+"""
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
