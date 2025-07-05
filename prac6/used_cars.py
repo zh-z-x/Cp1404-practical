dfc0182c628d5e7629e55132b4793b936c821295
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-05 12:24:30 +0800

    Added car name to constructor Added: Create new Car object named 'Lamborghini' with 100 fuel; Add 20 units of fuel to Lamborghini

--- prac6/used_cars.py
+++ prac6/used_cars.py
@@ -9,10 +9,14 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("MyCar", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+
+    lamborghini = Car("Lamborghini", 100)
+
+    lamborghini.add_fuel(20)
 
 
 main()
