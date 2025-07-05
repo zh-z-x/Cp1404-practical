47b6b9b32bec6c673f7987f61a4c23fb978c381e
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-05 13:08:30 +0800

    Added: init with default values

---
+++ prac6/guitar.py
@@ -0,0 +1,15 @@
+"""
+guitar.py
+
+Estimated time: 15 minutes
+
+Actual time:  minutes
+"""
+CURRENT_YEAR = 2025
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
