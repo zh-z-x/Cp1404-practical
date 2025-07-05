f1a3634d18c3c425e3617c6f6e7ef93bad31a2a8
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-05 12:15:43 +0800

    Added 'name' parameter with default value; Added docstring for new name parameter; Added: Store car name

--- prac6/car.py
+++ prac6/car.py
@@ -4,11 +4,12 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="Car", fuel=0):
         """Initialise a Car instance.
-
+        name: string, name of the car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
