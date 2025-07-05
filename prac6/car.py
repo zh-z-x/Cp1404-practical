c0467c40a0c2e90dd330dfc855373c6268a88510
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-05 12:18:18 +0800

    Added: Provide readable string output of car state

--- prac6/car.py
+++ prac6/car.py
@@ -30,3 +30,7 @@ class Car:
             self.fuel -= distance
         self._odometer += distance
         return distance
+
+    def __str__(self):
+        """Return string representation of the car."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
