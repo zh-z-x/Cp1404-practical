c60505e2277bc1c9e50ef897403b32cd9801a051
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-05 12:57:09 +0800

    Added: Import custom class from module; Create language objects with name, typing, reflection, year; Print one object to test __str__ method

---
+++ prac6/languages.py
@@ -0,0 +1,16 @@
+"""
+languages.py
+
+Estimated time: 25 minutes
+
+Actual time: minutes
+"""
+from programming_language import ProgrammingLanguage
+
+def main():
+
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
