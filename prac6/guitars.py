eeabc3217d3a3e7ca3fe0e30bf96b7cecbd1a20e
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-05 13:20:43 +0800

    Main program for managing a collection of guitars; Added test data.

---
+++ prac6/guitars.py
@@ -0,0 +1,16 @@
+"""
+guitars.py
+
+Estimated time: 15 minutes
+
+Actual time:  minutes
+"""
+from guitar import Guitar
+
+def main():
+    print("My guitars!")
+    guitars = []
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
