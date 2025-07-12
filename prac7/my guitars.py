42152e75ea5d4aeba966a760b9dc7c431cdc69db
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-12 12:26:58 +0800

    Add function to load guitars from guitar.csv into object list

--- prac7/my guitars.py
+++ prac7/my guitars.py
@@ -12,3 +12,18 @@ def main():
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+def load_guitars(filename):
+    guitars = []
+    in_file = open(filename, 'r')
+    for line in in_file:
+        parts = line.strip().split(',')
+        name = parts[0].strip()
+        year = int(parts[1])
+        cost = float(parts[2])
+        guitars.append(Guitar(name, year, cost))
+    in_file.close()
+    return guitars
+
+FILENAME = "guitar.csv"
+guitars = load_guitars(FILENAME)
