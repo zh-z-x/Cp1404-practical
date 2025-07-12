6f5541d85d34a7aff9c44fb163c42b4babdcdfb8
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-12 12:15:28 +0800

    prac 6 guitar.py

---
+++ prac7/guitar.py
@@ -0,0 +1,17 @@
+CURRENT_YEAR = 2025
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= 50
