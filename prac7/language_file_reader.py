39caec61bff1af3e8c0fc53d7de5c4fe9a37a5f7
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-12 11:55:19 +0800

    - Added reading and boolean conversion of PointerArithmetic field in main(). - Passed pointer_arithmetic parameter when constructing ProgrammingLanguage objects. - Updated using_namedtuple() and using_csv_namedtuple() functions to use 5 fields. - Kept using_csv() printing logic unchanged for raw row output.

--- prac7/language_file_reader.py
+++ prac7/language_file_reader.py
@@ -28,7 +28,8 @@ def main():
         reflection = parts[2] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        pointer_arithmetic = parts[4] == "Yes"
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
@@ -63,7 +64,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer_arithmetic')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
@@ -78,11 +79,12 @@ def using_namedtuple():
 
 def using_csv_namedtuple():
     """Language file reader version using both csv module and named tuple."""
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer_arithmetic')
     in_file = open("languages.csv", "r")
     in_file.readline()
     for language in map(Language._make, csv.reader(in_file)):
-        print(language.name, 'was released in', language.year)
+        print(language.name, 'was released in', language.year,
+              '| Pointer Arithmetic:', language.pointer_arithmetic)
         print(repr(language))
 
 # using_csv_namedtuple()
