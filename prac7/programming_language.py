5ee981a162ce9b01b1ab8639ac0578964ce4e329
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-12 11:54:35 +0800

    - Added pointer_arithmetic attribute to indicate if language supports pointer arithmetic. - Updated __init__ constructor to accept the new parameter. - Modified __repr__ method to display pointer_arithmetic status. - Updated run_tests() to include the new attribute in test objects.

--- prac7/programming_language.py
+++ prac7/programming_language.py
@@ -7,12 +7,13 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
