242b1a3b4ecb91e1735fd6fef29889f20000a8ce
zzx <zixin.zhang@my.jcu.edu.au>
2025-07-27 11:48:53 +0800

    Updated the Taxi class to use a class variable price_per_km shared by all taxis, and removed the parameter from the constructor.

--- prac9/taxi_test.py
+++ prac9/taxi_test.py
@@ -4,7 +4,7 @@ def main():
     """Test the Taxi class with a sample scenario."""
 
     # 1. Create a new Taxi object
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
 
     # 2. Drive the taxi 40 km
     my_taxi.drive(40)
